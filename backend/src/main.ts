7183300dddec4b1fa5c938ad2e735aa3cfd3ef1e
Lashkevich Aliaksandr <a.lashkevich93@gmail.com>
2025-06-11 23:13:25 +0300

    fix: added validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     methods: 'HEAD,GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
